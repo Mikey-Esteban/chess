3f53665a0e1c755756bf48ec324e2d35095dea10
Mikey Esteban <michaelp.esteban@gmail.com>
2020-06-28 16:50:39 -0400

    refactor board class out of game class

--- lib/board.rb
+++ lib/board.rb
@@ -1,85 +1,9 @@
-# create and populate an empty board
-require "./lib/chessmen.rb"
-require "./lib/pawn.rb"
-
 class Board
-  attr_accessor :board, :player_white, :player_black, :chessmen
+  attr_accessor :board
 
   def initialize
-    @board = {}   # position => ['board piece', occupancy]
-    @pieces = {
-      "rook"   => 'r'  ,
-      "knight" => 'kn' ,
-      "bishop" => 'b'  ,
-      "queen"  => 'Q'  ,
-      "king"   => 'K'  ,
-      "pawn"   => 'p'
-    }
-    @chessmen = Chessmen.new
-    @player_white = @chessmen.white
-    @player_black = @chessmen.black
+    @board = {}
     populate_board_on_init
-  end
-
-
-  def populate_board
-    chessmen_attributes = grab_chessmen_position_name
-    @board.each do |position, value_pair|     # value pair = [board piece, occupancy]
-      chessmen_attributes.each do |piece_attr|
-        if piece_attr.include?(position)
-          value_pair[1] = piece_attr[1]   # occupancy = name
-          value_pair[0] = place_chessmen(value_pair[0].length, piece_attr[1], value_pair[0])
-        end
-      end
-    end
-    print_board
-    @board
-  end
-
-  private
-
-  def grab_chessmen_position_name
-    players = [self.player_white, self.player_black]
-    attributes = []
-    players.each do |player|
-      player.each do |type, pieces|
-        pieces.each do |piece|
-          name = piece.name
-          position = piece.position
-          attributes << [position, name]
-        end
-      end
-    end
-    return attributes
-  end
-
-  def place_chessmen(length, type, board_piece)
-    if length == 3
-      if type == "knight"   # special case for knight -> 'kn'
-        first = board_piece[0]
-        second = @pieces[type][0]
-        third = @pieces[type][1]
-      else
-        first = board_piece[0]
-        second = @pieces[type]
-        third = board_piece[2]
-      end
-      result = first+second+third
-    else
-      if type == "knight"
-        first = board_piece[0]
-        second = board_piece[1]
-        third = @pieces[type][0]
-        fourth = @pieces[type][1]
-      else
-        first = board_piece[0]
-        second = board_piece[1]
-        third = @pieces[type]
-        fourth = board_piece[3]
-      end
-      result = first+second+third+fourth
-    end
-    result
   end
 
   def populate_board_on_init
@@ -125,6 +49,3 @@ class Board
   end
 
 end
-
-board = Board.new
-board.populate_board
