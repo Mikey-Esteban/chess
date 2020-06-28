8afc69a28b767617e13c229b336f87ced1a83e77
Mikey Esteban <michaelp.esteban@gmail.com>
2020-06-28 16:51:47 -0400

    put pawn class back in

--- lib/chessmen.rb
+++ lib/chessmen.rb
@@ -71,29 +71,15 @@ class Chessmen
 
 end
 
-# class Pawn < Chessmen
-#   attr_accessor :position, :name, :color
-#
-#   def initialize(position, color)
-#     @name = 'pawn'
-#     @position = position
-#     @color = color
-#   end
-#
-#   def move
-#     if move_valid?
-#       @position[1] = (@position[1].to_i + 1).to_s if @color == 'white'
-#       @position[1] = (@position[1].to_i - 1).to_s if @color == 'black'
-#     else
-#       puts "Sorry cant move pawn there"
-#       nil
-#     end
-#   end
-#
-#   def take(to_position)
-#     @position = to_position
-#   end
-# end
+class Pawn < Chessmen
+  attr_accessor :position, :name, :color
+
+  def initialize(position, color)
+    @name = 'pawn'
+    @position = position
+    @color = color
+  end
+end
 
 class Rook < Chessmen
   attr_accessor :position, :name, :color
