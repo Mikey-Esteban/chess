958c0f00522af2ae7f2370a54b356a3ca000cbec
Mikey Esteban <michaelp.esteban@gmail.com>
2020-06-28 11:45:45 -0400

    del func fill_pieces, refactor Board class

--- lib/board.rb
+++ lib/board.rb
@@ -17,39 +17,22 @@ class Board
     @chessmen = Chessmen.new
     @player_white = @chessmen.white
     @player_black = @chessmen.black
+    populate_board_on_init
   end
 
 
   def populate_board
-    populate_board_no_pieces
     chessmen_attributes = grab_chessmen_position_name
     @board.each do |position, value_pair|     # value pair = [board piece, occupancy]
-      # p position, value_pair
       chessmen_attributes.each do |piece_attr|
         if piece_attr.include?(position)
           value_pair[1] = piece_attr[1]   # occupancy = name
           value_pair[0] = place_chessmen(value_pair[0].length, piece_attr[1], value_pair[0])
         end
       end
-      p position, value_pair
     end
-    show_board_no_pieces
+    print_board
     @board
-    #   if key.include?('7') || key.include?('2')
-    #     if value_pair[0].length == 3
-    #       # WERE HERE !!!!!!!!!!!!
-    #       board[key] = place_chessmen(3, "pawn", board[key])
-    #     else
-    #       board[key] = place_chessmen(4, "pawn", board[key])
-    #     end
-    #   elsif key.include?('8')
-    #     fill_pieces(key)
-    #   elsif key.include?('1')
-    #     fill_pieces(key)
-    #   end
-    # end
-    # show_board_no_pieces
-    # @board
   end
 
   private
@@ -62,28 +45,11 @@ class Board
         pieces.each do |piece|
           name = piece.name
           position = piece.position
-          # color = piece.color
           attributes << [position, name]
         end
       end
     end
     return attributes
-  end
-
-  def fill_pieces(key)
-    if key.include?('a')
-      board[key] = place_chessmen(3, "rook", board[key])
-    elsif key.include?('b') || key.include?('g')
-      board[key] = place_chessmen(4, "knight", board[key])
-    elsif key.include?('c') || key.include?('f')
-      board[key] = place_chessmen(4, "bishop", board[key])
-    elsif key.include?('d')
-      board[key] = place_chessmen(4, "queen", board[key])
-    elsif key.include?('e')
-      board[key] = place_chessmen(4, "king", board[key])
-    elsif key.include?('h')
-      board[key] = place_chessmen(4, "rook", board[key])
-    end
   end
 
   def place_chessmen(length, type, board_piece)
@@ -115,7 +81,7 @@ class Board
     result
   end
 
-  def populate_board_no_pieces
+  def populate_board_on_init
     row = [8,7,6,5,4,3,2,1]
     col = ['a','b','c','d','e','f','g','h']
     white = "|   "
@@ -137,20 +103,19 @@ class Board
         end
       end
     end
-    # @board = @board.transform_keys(&:to_sym)
     @board
   end
 
-  def show_board_no_pieces
+  def print_board
     puts
     top_axis = "       a   b   c   d   e   f   g   h\n\n"
     bottom_axis = "\n       a   b   c   d   e   f   g   h"
     result = ""
     result += top_axis
-    @board.each do |k,v|
-      result += "  #{k[1]}   " + v[0] if k.include?('a')
-      result += v[0] if !k.include?('a') && !k.include?('h')
-      result += v[0] + "    #{k[1]}\n" if k.include?('h')
+    @board.each do |k, v|     # v = [board_piece, occupancy]
+      result += "  #{k[1]}   " +  v[0] if k.include?('a')
+      result +=  v[0] if !k.include?('a') && !k.include?('h')
+      result +=  v[0] + "    #{k[1]}\n" if k.include?('h')
     end
     result += bottom_axis
     puts result
@@ -162,5 +127,3 @@ end
 
 board = Board.new
 board.populate_board
-# chessmen_attributes = board.grab_chessmen_position_name
-# p chessmen_attributes
