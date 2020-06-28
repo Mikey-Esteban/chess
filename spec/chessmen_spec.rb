1e4048f8509a959b010ce48940cb08769e30ea25
Mikey Esteban <michaelp.esteban@gmail.com>
2020-06-28 16:52:30 -0400

    delete pawn tests

--- spec/chessmen_spec.rb
+++ spec/chessmen_spec.rb
@@ -1,27 +1,5 @@
 require './lib/chessmen.rb'
 
 describe Chessmen do
-  describe Pawn do
-    describe '#move' do
-      it 'moves a pawn up one spot if white' do
-        pawn = Pawn.new('a2', 'white')
-        pawn.move
-        expect(pawn.position).to eq('a3')
-      end
-
-      it 'moves a pawn up down one spot if black' do
-        pawn = Pawn.new('c7', 'black')
-        pawn.move
-        expect(pawn.position).to eq('c6')
-      end
-    end
-
-    describe '#take' do
-      it 'moves a pawn diagonal to eat a piece' do
-        pawn = Pawn.new('b2', 'white')
-        pawn.take('a3')
-        expect(pawn.position).to eq('a3')
-      end
-    end
-  end
+  
 end
