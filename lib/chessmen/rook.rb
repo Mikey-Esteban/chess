ccc8686bd875900159468b1df454a7b5b77eff45
Mikey Esteban <michaelp.esteban@gmail.com>
2020-06-29 18:38:20 -0400

    refactor func check_next_square

--- lib/chessmen/rook.rb
+++ lib/chessmen/rook.rb
@@ -16,17 +16,17 @@ class Rook
 
     return "#{self.name} cant make a diagonal move" if diagonal
 
-    false
+    true
   end
 
   def check_next_square(start_square, end_square)
-    column_diff =  start_square[0] != end_square[0] if start_square[0] != end_square[0]
     row_diff = start_square[1] != end_square[1] if start_square[1] != end_square[1]
+    column_diff =  start_square[0] != end_square[0] if start_square[0] != end_square[0]
     next_square = end_square
     p "next square before: #{next_square}"
     if row_diff
-      next_square[1] = (end_square[1].to_i - 1).to_s if start_square[1] > end_square[1]
-      next_square[1] = (end_square[1].to_i + 1).to_s if start_square[1] < end_square[1]
+      next_square[1] = (end_square[1].to_i - 1).to_s if start_square[1] < end_square[1]
+      next_square[1] = (end_square[1].to_i + 1).to_s if start_square[1] > end_square[1]
     elsif column_diff
       next_square[0] = (end_square[0].ord - 1).chr if start_square[0].ord < end_square[0].ord
       next_square[0] = (end_square[0].ord + 1).chr if start_square[0].ord > end_square[0].ord
