d4ece5cd18e0777c17e54e9205d0d41df89f839a
Mikey Esteban <michaelp.esteban@gmail.com>
2020-06-29 18:20:38 -0400

    add func check_next_square

--- lib/chessmen/rook.rb
+++ lib/chessmen/rook.rb
@@ -6,4 +6,31 @@ class Rook
     @position = position
     @color = color
   end
+
+  def move_valid?(start_square, end_square)
+    p "In Rook.move_valid Instance method!"
+    column_diff =  start_square[0] != end_square[0] if start_square[0] != end_square[0]
+    row_diff = start_square[1] != end_square[1] if start_square[1] != end_square[1]
+
+    diagonal = column_diff && row_diff
+
+    return "#{self.name} cant make a diagonal move" if diagonal
+
+    false
+  end
+
+  def check_next_square(start_square, end_square)
+    column_diff =  start_square[0] != end_square[0] if start_square[0] != end_square[0]
+    row_diff = start_square[1] != end_square[1] if start_square[1] != end_square[1]
+    next_square = end_square
+    p "next square before: #{next_square}"
+    if row_diff
+      next_square[1] = (end_square[1].to_i - 1).to_s if start_square[1] > end_square[1]
+      next_square[1] = (end_square[1].to_i + 1).to_s if start_square[1] < end_square[1]
+    elsif column_diff
+      next_square[0] = (end_square[0].ord - 1).chr if start_square[0].ord < end_square[0].ord
+      next_square[0] = (end_square[0].ord + 1).chr if start_square[0].ord > end_square[0].ord
+    end
+    next_square
+  end
 end
