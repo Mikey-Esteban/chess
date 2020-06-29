0359da472feb251a13fac82350f45c797334648f
Mikey Esteban <michaelp.esteban@gmail.com>
2020-06-29 11:02:52 -0400

    working functionality to Pawn Class func move_valid? & pawn_take_valid?

--- lib/chessmen.rb
+++ lib/chessmen.rb
@@ -82,10 +82,21 @@ class Pawn < Chessmen
 
   def move_valid?(start_square, end_square)
     p "In Pawn.move_valid Instance method!"
-    diff = end_square[1].to_i - start_square[1].to_i  if @color == 'white'
-    diff = end_square[1].to_i  - start_square[1].to_i  if @color == 'black'
+    diff = end_square[1].to_i - start_square[1].to_i
 
-    diff == 1 || diff == -1 ? true : false
+    return false if start_square[0] != end_square[0]
+    return diff == 1 if self.color == 'white'
+    return diff == -1 if self.color == 'black'
+  end
+
+  def pawn_take_valid?(start_square, end_square)
+    p "In Pawn.pawn_take_valid Instance method!"
+    column_diff = end_square[0].ord - start_square[0].ord
+    row_diff = end_square[1].to_i - start_square[1].to_i
+
+    return false unless column_diff == 1 || column_diff == -1
+    return row_diff == 1 if self.color == 'white'
+    return row_diff == -1 if self.color == 'black'
   end
 end
 
